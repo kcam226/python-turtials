511f85b08abab7de185c867560754f7d8223e7c5
kcam226 <soulcam226@gmail.com>
2021-11-18 07:30:54 -0500

    starting coders journey

---
+++ assignments/ass01.py
@@ -0,0 +1,36 @@
+print("The pc will ask you 5 questions, answer them correctly")
+
+# Question 1 ################################
+question01 = input("Is this a two digit no. : 12? [yes/no]" )
+if question01 == "yes":
+    print("You are right!!")
+else:
+    print("You are wrong!!")
+
+# Question 2 ################################
+question02 = input("Is india largest democratic country? [yes/no]: " )
+if question02 == "yes":
+    print("You are right!!")
+else:
+    print("You are wrong!!")
+
+# Question 3 ################################
+question03 = input("Is snowfall existing in sahara desert? [yes/no]: " )
+if question03 == "no":
+    print("You are right!!")
+else:
+    print("You are wrong!!")
+
+# Question 4 ################################
+question04 = input("National sport of india is football? [yes/no]: " )
+if question04 == "no":
+    print("You are right!!")
+else:
+    print("You are wrong!!")
+
+# Question 5 ################################
+question05 = input("national flower of india is rose? [yes/no]: " )
+if question05 == "yes":
+    print("You are right!!")
+else:
+    print("You are wrong!!")
